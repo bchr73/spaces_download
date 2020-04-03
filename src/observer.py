00583001b362a3b661ae6c51b6222b3e304a47f0
Karim Boucher <boucherkarim@gmail.com>
2020-04-02 21:31:14 -0400

    Update observers

--- src/observer.py
+++ src/observer.py
@@ -3,52 +3,55 @@ from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from download import Download
+    from task import Task, Download
+    from typing import Type, List
 
-class Observer(ABC):
+class Observer:
     """The Obvserver interface declares the update method, used by downloads.
     """
 
-    @abstractmethod
-    def __init__(self, callback) -> None:
-        pass
+    def __init__(self, callback=None) -> None:
+        self._callback = callback
 
-    @abstractmethod
-    def update(self, download: Download) -> None:
-        """Receive update from download.
+    def update(self, task: Type[Task]) -> None:
+        """Receive update from task.
         """
-        pass
+        raise NotImplementedError
 
-class DownloadCompleteObserver(Observer):
-    """Observer, reacts to download transferred bytes == total bytes.
-    """
-
-    def __init__(self, callback) -> None:
-        self.callback = callback
-
-    def update(self, download: Download) -> None:
-        if download._bytes_transferred == download._size:
-            self.callback(download)
-            print(f'Download {download.id} complete.')
-
-class Observable(ABC):              
+class Observable:
     """Interface declaring methods for managing subscribers.                                                                                                                  
     """                   
 
-    @abstractmethod
-    def attach(self, observer: Observer) -> None:                        
+    def __init__(self):
+        self._observers: List = []
+
+    def attach(self, observer: Observer) -> None:
         """Attach an observer to the subscriber.                                                                                                                              
-        """                                                       
-        pass                                                         
-                                                                     
-    @abstractmethod                                                         
-    def detach(self, observer: Observer) -> None:                    
+        """
+        if not observer in self._observers:
+            self._observers.append(observer)
+
+    def detach(self, observer: Observer) -> None:
         """Detach an observer from the subscriber.                                                                                                                            
         """
-        pass                             
-                                                                                                                                                                              
-    @abstractmethod                                                                                                                                                           
-    def notify(self) -> None:                                                                                                                                                 
+        if observer in self._observers:
+            self._observers.remove(observer)
+
+    def notify(self) -> None:
         """Notify all observers about an event.                                                                                                                               
         """
-        pass
+        for observer in self._observers:
+            observer.update(self)
+
+class DownloadCompleteObserver(Observer):
+    """Moves Download to complete_queue when fully transferred.
+    """
+
+    def update(self, download: Type[Download]) -> None:
+        if download._bytes_transferred == download._size:
+            return self._callback()
+
+class ProgressObserver(Observer):
+    """Updates progress_map with progress information.
+    """
+    pass
